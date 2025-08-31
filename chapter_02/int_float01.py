8870c3ada9be66b6c5579900f5afd38965f165e9
mrjanggg <ijin1322@gmail.com>
2025-08-31 19:10:34 +0900

    Initial commit

---
+++ chapter_02/int_float01.py
@@ -0,0 +1,5 @@
+output_a = int("52")
+output_b = float("52.1231323")
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
---
+++ chapter_02/str.py
@@ -0,0 +1,5 @@
+output_a = str(52)
+output_b = str(52.2334)
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
