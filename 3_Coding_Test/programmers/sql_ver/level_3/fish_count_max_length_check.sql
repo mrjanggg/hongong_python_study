82b183b6c1100a24245143abaf5670f484538270
mrjanggg <ijin1322@gmail.com>
2025-11-06 00:04:58 +0900

    코드 수정

--- 3_Coding_Test/programmers/sql_ver/level_3/fish_count_max_length_check.sql
+++ 3_Coding_Test/programmers/sql_ver/level_3/fish_count_max_length_check.sql
@@ -1,5 +1,3 @@
--- 파일명: fish_count_max_length_check.sql
-
 -- 문제: 프로그래머스 lv.3 '특정 조건을 만족하는 물고기별 수와 최대 길이 구하기'
 -- 설명: FISH_INFO 테이블에서 물고기 종류별로 잡은 수, 최대 길이, 물고기 종류를 조회해야 한다.
 --       이때, 길이가 NULL(10cm 이하)인 물고기는 10cm로 취급해야 하며,
@@ -8,20 +6,28 @@
 -- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/298519
 
 -- 풀이:
--- 1. 길이(LENGTH)가 NULL인 경우(10cm 이하)를 10으로 처리하기 위해 IFNULL(LENGTH, 10) 함수를 사용한다.
--- 2. FISH_TYPE별로 그룹화(GROUP BY)하여 잡은 수(COUNT(*))와 IFNULL 처리된 최대 길이(MAX)를 구한다.
+-- 1. 길이(LENGTH)가 NULL인 경우를 10으로 처리하기 위해 IFNULL 함수 대신 CASE 문을 사용하여 명시적으로 조건을 분기한다.
+-- 2. FISH_TYPE별로 그룹화(GROUP BY)하여 잡은 수(COUNT(*))와 NULL 처리된 최대 길이(MAX)를 구한다.
 -- 3. 그룹 함수(AVG)를 사용한 조건(평균 길이 >= 33)은 HAVING 절을 사용하여 필터링한다.
 -- 4. 결과를 FISH_TYPE 오름차순(ORDER BY)으로 정렬한다.
 
 SELECT
     COUNT(*) AS FISH_COUNT,
-    MAX(IFNULL(LENGTH, 10)) AS MAX_LENGTH,
+    MAX(
+        CASE
+            WHEN LENGTH IS NULL THEN 10
+            ELSE LENGTH
+        END) AS MAX_LENGTH,
     FISH_TYPE
 FROM
     FISH_INFO
 GROUP BY
     FISH_TYPE
 HAVING
-    AVG(IFNULL(LENGTH, 10)) >= 33
+    AVG(
+        CASE
+            WHEN LENGTH IS NULL THEN 10
+            ELSE LENGTH
+        END) >= 33
 ORDER BY
     FISH_TYPE ASC;
