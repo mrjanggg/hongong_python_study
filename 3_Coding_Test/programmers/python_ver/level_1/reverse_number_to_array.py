13e7eeb8768f245caafb880766e2b8fa0fc20cc9
mrjanggg <ijin1322@gmail.com>
2025-11-29 23:57:10 +0900

    프로그래머스 레벨1 Python 코드 수정

--- 3_Coding_Test/programmers/python_ver/level_1/reverse_number_to_array.py
+++ 3_Coding_Test/programmers/python_ver/level_1/reverse_number_to_array.py
@@ -1,29 +1,24 @@
-# 문제: 프로그래머스 Lv.1 '자연수 뒤집어 배열로 만들기'
-# 설명: 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 
-#       예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+# 문제: 프로그래머스 lv.1 '자연수 뒤집어 배열로 만들기'
+# 설명: 자연수 n을 입력받아, 각 자릿수를 뒤집은 순서대로 배열에 담아 반환해야 한다.
+#       예를 들어, n=12345 이면 [5, 4, 3, 2, 1]을 반환한다.
 # 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12932
 
-# 풀이: 자연수 n을 문자열로 변환한 뒤, 길이를 구해 자릿수를 파악합니다.
-#       range()와 for 반복문을 사용하여 자릿수만큼 반복하면서,
-#       문자열 인덱싱(str_n[-(k+1)])을 이용해 맨 뒤 글자부터 거꾸로 접근합니다.
-#       접근한 문자를 int()로 숫자로 바꾸어 빈 리스트 answer에 차례대로 추가합니다.
-
+# 풀이:
+# 1. 자연수 n을 인덱싱이 가능한 문자열(str_n)로 변환한다.
+# 2. 결과 리스트(answer)를 생성한다.
+# 3. 문자열의 가장 마지막 인덱스(len(str_n) - 1)부터 0까지 역순으로 순회한다.
+# 4. 각 자릿수(문자)를 정수형(int)으로 변환하여 리스트에 추가(append)한다.
 
 def solution(n):
-       
+    	
     #변수값 초기 세팅
     answer = [ ]
-    digit_number = 0
     
     # 숫자를 문자열로 변환
     str_n = str(n)
     
-    # 자릿수 파악
-    for i in str_n:
-        digit_number += 1
-    
-    #빈 리스트에 역순으로 값 넣기
-    for k in range(digit_number):
-        answer.append(int(str_n[-(k+1)]))
-    
+    # 문자열로 된 배열의 역순을 answer에 저장.
+    for i in range(len(str_n) - 1, 0 - 1, -1):
+        answer.append(int(str_n[i]))
+
     return answer
